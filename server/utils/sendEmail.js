e4125db4e87b2e73d206ecb6c4265b5d2598a711
Avinash <www.kumaravinash3898@gmail.com>
2026-03-23 23:46:36 +0530

    update emailtempls.js

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: false, // 587 ke liye false
+  secure: true, // 587 ke liye false
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
